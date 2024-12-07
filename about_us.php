3e35fb609f6c4b4599c41c245aa8c2ab1d7c198e
Thamasha Devindi <vindi835@gmail.com>
2024-12-07 11:04:32 +0530

    Initial commit

---
+++ about_us.php
@@ -0,0 +1,46 @@
+<html lang="en" data-bs-theme="dark">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php include 'nav.php'; ?>
+
+    <div class="container">
+        <div class="jumbotron text-center mt-3">
+            <h1 class="display-4">Who we Are?</h1>
+            <p class="lead">Film Mania movie site primarily allows users to easily find a movie they want from a wide range of movies as these 
+                movies are categorized on different basis. The target audience for the movie site are the movie enthusiasts who are excited 
+                in exploring and discovering new movies to watch. And also, for the people who are looking for information about movies that 
+                they are interested in will also be benefited. Other than for the movie enthusiasts, this site will also be for the filmmakers 
+                and producers to engage with their audience, to showcase their works, promote their movies and to take feedback on their movies. </p>
+                
+                <hr class="my-4">
+
+            <h1 class="display-4">Contact Us</h1>
+            <p>
+            Do you have something you’d like to let us know? Whether you have a comment or an idea to share, we look forward to hearing from 
+            you: <a href="#">feedback@filmmania.com</a>
+            </p>
+
+            <p>For press inquiries, email us at <a href="#">press@filmmania.com</a></p>
+
+            <p>You can also engage with us on <a href="#">Twitter</a>, <a href="#">Facebook</a>, and <a href="#">LinkedIn</a>.</p>
+
+            <p>If you would rather call us or send us a letter, you can reach us at Passara Road, Badulla, Sri Lanka. | 
+                +9471 2345678.</p>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
